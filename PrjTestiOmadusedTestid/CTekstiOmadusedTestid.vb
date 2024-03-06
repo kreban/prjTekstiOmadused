e36cfcb06905e40af16a4cfcd3e2192083b068b1
Karoliina Rebane <kreban@taltech.ee>
2024-03-06 14:49:07 +0200

    Ekraanivormi testimine

--- PrjTestiOmadusedTestid/CTekstiOmadusedTestid.vb
+++ PrjTestiOmadusedTestid/CTekstiOmadusedTestid.vb
@@ -5,11 +5,21 @@ Imports Microsoft.VisualStudio.TestTools.UnitTesting
 
     <DataRow("aba", 2)>
     <DataRow("pxklLK", 0)>
+    <DataRow("ttt", 0)>
+    <DataRow("aaa", 3)>
+    <DataRow("abcddd", 1)>
+    <DataRow("aEiOxX", 4)>
     <DataTestMethod()> Public Sub LoeTaishaalikuteArv_AndmepohineTest(sisend, oodatavVastus)
         Dim testija As New PrjTekstiOmadused.CTekstiOmadused(sisend)
         Dim vastus = testija.intTaishaalikuteArv
 
         Assert.AreEqual(oodatavVastus, vastus)
+    End Sub
+
+    <TestMethod()> Public Sub KontrolliPaliondroom_SonaAnna_VastusToene()
+        Dim testija As New PrjTekstiOmadused.CTekstiOmadused("anna")
+        Dim vastus = testija.boolPalindroom
+        Assert.IsTrue(vastus)
     End Sub
 
 End Class
---
+++ PrjTestiOmadusedTestid/EkraaniVormTestid.vb
@@ -0,0 +1,17 @@
+﻿Imports System.Text
+Imports Microsoft.VisualStudio.TestTools.UnitTesting
+Imports PrjTekstiOmadused
+
+<TestClass()> Public Class EkraaniVormTestid
+
+    <DataRow("ada", "Jah")>
+    <DataRow("adaa", "Ei")>
+    <DataTestMethod()> Public Sub KontrolliEkraanivormiPalindroom_AndmepohineTest(sisend, oodatavVastus)
+        Dim testija As New CTekstiOmadused(sisend)
+        Dim vorm As New EkraaniVorm
+        Dim vastus = vorm.PalindroomVastus(testija.KontrolliPalindroom(sisend))
+
+        Assert.AreEqual(oodatavVastus, vastus)
+    End Sub
+
+End Class
